5bec618db7ffa7fc65025061618c5b00902a8ba2
Kukvly <kukvly96@gmail.com>
2023-06-08 00:41:47 +0900

    react003git add .

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import pracComponent from "./pracComponent";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      <pracComponent></pracComponent>
     </div>
   );
 }
---
+++ reactprac/react001/src/pracComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class pracComponent extends Component{
+    render(){
+        return(
+            <h2>[THIS IS PRACTICE COMPONENT]</h2>
+        )
+    }
+}
+
+export default pracComponent;
