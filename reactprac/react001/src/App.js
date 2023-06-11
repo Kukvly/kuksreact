2d14371002ac7e8225727f3eb5ad67d6c2523501
Kukvly <kukvly96@gmail.com>
2023-06-11 17:52:27 +0900

    R004 20230611

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import './App.css';
 
 // import 경고 알아볼 것
-import pracComponent from "./kuksPracComponent";
+import ImportComponent from "./R003_ImportComponent";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
       <p>HTML 적용하기</p>
-      <pracComponent
-        prop_value = 'FromApp.js'></pracComponent>
+      <ImportComponent></ImportComponent>
     </div>
   );
 }
---
+++ reactprac/react001/src/R003_ImportComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class R003_ImportComponent extends Component{
+
+    render(){
+        // console.log("3. render Call");
+        return(
+            <h2>[THIS IS PRACTICE COMPONENT]</h2>
+        )
+    }
+}
+
+export default R003_ImportComponent;
