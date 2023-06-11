afb076b5a37246506bdb12f6881eb16580445247
Kukvly <kukvly96@gmail.com>
2023-06-11 18:09:36 +0900

    R004 20230611

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 // import 경고 알아볼 것
 import ImportComponent from "./R003_ImportComponent";
 
+// import LifecycleEx from './R004_LifecycleEx';
+import LifecycleEx from './R005_LifecycleEx';
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <p>HTML 적용하기</p>
-      <ImportComponent></ImportComponent>
+      {/* <p>HTML 적용하기</p> */}
+      <LifecycleEx
+        prop_value = 'FromApp.js'
+      />
     </div>
   );
 }
