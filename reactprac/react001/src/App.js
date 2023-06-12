2232e210193c393494485e57564fbde1020c5a15
Kukvly <kukvly96@gmail.com>
2023-06-12 23:53:17 +0900

    file modify

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 // import LifecycleEx from './R006_LifecycleEx';
 // import LifecycleEx from './R007_LifecycleEx';
 // import LifecycleEx from './R008_LifecycleEx';
-import Es6 from './R009_Es6';
-
+// import Es6 from './R009_Es6';
+import Variable from './R010_var';
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Es6/>
+      <Variable/>
     </div>
   );
 }
