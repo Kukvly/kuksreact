660f0a914296a981b61e20cfd83bc7a88d15bb74
Kukvly <kukvly96@gmail.com>
2023-06-13 23:06:27 +0900

    R011_20230613

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -8,13 +8,15 @@ import './App.css';
 // import LifecycleEx from './R007_LifecycleEx';
 // import LifecycleEx from './R008_LifecycleEx';
 // import Es6 from './R009_Es6';
-import Variable from './R010_var';
+// import Variable from './R010_var';
+import SpreadOperator from './R011_SpreadOperator';
+
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Variable/>
+      <SpreadOperator/>
     </div>
   );
 }
