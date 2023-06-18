ff154e2402d150c96f2a97046b992d50318471f6
Kukvly <kukvly96@gmail.com>
2023-06-18 21:23:28 +0900

    edit R013_20230618

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -10,15 +10,15 @@ import './App.css';
 // import Es6 from './R009_Es6';
 // import Variable from './R010_var';
 // import SpreadOperator from './R011_SpreadOperator';
-
-import ClassPrototype from './R012_ClassPrototype';
+// import ClassPrototype from './R012_ClassPrototype';
+import ArrowFunc from './R013_ArrowFunction';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ClassPrototype/>
+      <ArrowFunc/>
     </div>
   );
 }
