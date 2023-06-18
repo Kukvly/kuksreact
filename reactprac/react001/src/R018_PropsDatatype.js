54762ebb49484ec3e09df5a8467a58cc3caf3bf3
Kukvly <kukvly96@gmail.com>
2023-06-19 00:52:58 +0900

    edit R018_20230619_2

--- reactprac/react001/src/R018_PropsDatatype.js
+++ reactprac/react001/src/R018_PropsDatatype.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import datatype from 'prop-types';
 
 class R018_PropsDatatype extends Component {
-    render(){
+    render() {
         let {
-            String, Number, Boolean, Array, ObjectJson, Function
+            String,
+            Number,
+            Boolean,
+            Array,
+            ObjectJson,
+            Function
         } = this.props;
-        
-        return (
-            <div style={{padding: '0px'}}>
 
+        return (
+            <div style={{
+                    padding: '0px'
+                }}>
+                <p>StringProps: {String}</p>
+                <p>NumberProps: {Number}</p>
+                <span>BooleanProps: {Boolean.toString()}</span>
+                <p>ArrayProps: {Array.toString()}</p>
+                <p>Object JsonProps: {JSON.stringify(ObjectJson)}</p>
+                <p>FunctionProps: {Function}</p>
             </div>
         )
     }
 }
 
 R018_PropsDatatype.propTypes = {
-
+    String: datatype.number,
+    Number: datatype.number,
+    Boolean: datatype.bool,
+    Array: datatype.array,
+    ObjectJSON: datatype.object,
+    Function: datatype.func
 }
 
 export default R018_PropsDatatype;
