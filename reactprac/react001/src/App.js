9990af32b107b1660b47d4b9605b16f16ac7d915
Kukvly <kukvly96@gmail.com>
2023-06-19 23:34:26 +0900

    edit R019_20230619

--- reactprac/react001/src/App.js
+++ reactprac/react001/src/App.js
@@ -14,23 +14,32 @@ import './App.css';
 // import ArrowFunc from './R013_ArrowFunction';
 // import ForEach from './R014_ForEach';
 // import Props from './R017_Props';
-import PropsDatatype from './R018_PropsDatatype';
+//import PropsDatatype from './R018_PropsDatatype';
+import PropsBoolean from './R019_PropsBoolean';
 
 function App() {
+  // return (
+  //   <div>
+  //     <h1>Start React 200!</h1>
+  //     <p>CSS 적용하기</p>
+  //     <PropsDatatype
+  //       String='react'
+  //       Number={200}
+  //       Boolean={1==1}
+  //       Array={[0, 1, 8]}
+  //       Object Json={{react: '리액트', twohundred:'200'}}
+  //       Function={console.log('FunctionProps: function!')}
+  //     />
+  //   </div>
+  // );
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDatatype
-        String='react'
-        Number={200}
-        Boolean={1==1}
-        Array={[0, 1, 8]}
-        Object Json={{react: '리액트', twohundred:'200'}}
-        Function={console.log('FunctionProps: function!')}
-      />
+      <PropsBoolean BooleanTrueFalse={false}/>
+      <PropsBoolean BooleanTrueFalse/>
     </div>
-  );
+  )
 }
 
 export default App;
